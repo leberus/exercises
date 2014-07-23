90b5132bf81e046e55b546346f73c46c7eb39cba
leber <leber@paradise.cadinor.local>
2014-07-23 23:43:32 +0200

    Fix mynet

--- net1/mynet1.c
+++ net1/mynet1.c
@@ -2,25 +2,12 @@
 #include <linux/module.h>
 #include <linux/netdevice.h>
 
-static void mysetup(struct net_device *mydev);
-static int mynet_open(struct net_device *mydev);
-static int mynet_close(struct net_device *mydev);
+static void mysetup(struct net_device *d);
+static int mynet_open(struct net_device *p);
+static int mynet_close(struct net_device *b);
 
-struct mynet_struct
-{
-	int a;
-	int b;
-};
-
-static struct mynet_struct mydev_t; 
 static struct net_device *mydev;
 
-static void mysetup(struct net_device *d)
-{
-	ether_setup (d);
-/*	d->netdev_ops.ndo_open = mynet_open;
-	d->netdev_ops.ndo_stop = mynet_close; */
-}
 
 static int mynet_open(struct net_device *p)
 {
@@ -34,23 +21,42 @@ static int mynet_close(struct net_device *b)
 	return 0;
 }
 
-static __init int my_init(void)
+static struct net_device_ops ndo = {
+        .ndo_open = mynet_open,
+        .ndo_stop = mynet_close,
+        //.ndo_start_xmit = stub_start_xmit,
+};
+
+static void mysetup(struct net_device *d)
+{
+        int j;
+
+                for(j = 0 ; j < ETH_ALEN ; j++)
+                        d->dev_addr[j] = (char)j;
+
+                ether_setup (d);
+		d->netdev_ops = &ndo;
+}
+
+
+
+static int __init my_init(void)
 {
 	int ret = 0;
 
-		mydev = alloc_netdev(sizeof(mydev_t), "mynet1", mysetup);
-
-		if( mydev )
-			ret = register_netdev(mydev);
-		else {
-			pr_info("Failed allocating netdev");
+		mydev = alloc_netdev(0, "mynet%d", mysetup);
+	
+		if( register_netdev(mydev) ) {
+			pr_debug("Failed allocating netdev");
+			free_netdev(mydev);
 			ret = -EINVAL;
-		}
+		}	
+		pr_debug("Sucees!\n");
 
 		return ret;		
 }
 
-static __exit void my_exit(void)
+static void __exit my_exit(void)
 {
 	unregister_netdev(mydev);
 	free_netdev(mydev);
