a73f5538f41d3d6c790648c1ba08d21f762ede57
Oscar Salvador Vilardaga <os@iphh.net>
2014-07-24 17:54:44 +0200

    hola

--- net1/mynet1.c
+++ net1/mynet1.c
@@ -1,6 +1,8 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/netdevice.h>
+#include <linux/kallsyms.h>
+#include <linux/kprobes.h>
 
 static void mysetup(struct net_device *d);
 static int mynet_open(struct net_device *p);
@@ -8,6 +10,25 @@ static int mynet_close(struct net_device *b);
 
 static struct net_device *mydev;
 
+/*
+	Debugging purposes
+*/
+
+static struct jprobe jp;
+static const char *f_to_debug = "register_netdev";
+
+static int debugof_register_netdev( struct net_device *dev )
+{
+
+	pr_info("Here!\n");
+	if( dev->netdev_ops == NULL )
+		pr_info("It's NULL\n");
+	else
+		pr_info("It's NOT NULL\n");
+
+	jprobe_return();
+	return 0;
+}
 
 static int mynet_open(struct net_device *p)
 {
@@ -36,6 +57,8 @@ static void mysetup(struct net_device *d)
 
                 ether_setup (d);
 		d->netdev_ops = &ndo;
+
+		pr_info("Setted up ( %s )\n", d->name );
 }
 
 
@@ -43,15 +66,21 @@ static void mysetup(struct net_device *d)
 static int __init my_init(void)
 {
 	int ret = 0;
+	
+		jp.kp.addr = (kprobe_opcode_t *)register_netdev;
+		jp.entry = (kprobe_opcode_t *)kallsyms_lookup_name("debugof_register_netdev");
+		register_jprobe(&jp);
 
 		mydev = alloc_netdev(0, "mynet%d", mysetup);
+
+		pr_info("Addr of register_netdev: %x\n", (int)jp.kp.addr);
 	
 		if( register_netdev(mydev) ) {
 			pr_debug("Failed allocating netdev");
 			free_netdev(mydev);
 			ret = -EINVAL;
 		}	
-		pr_debug("Sucees!\n");
+		pr_info("Sucees!\n");
 
 		return ret;		
 }
@@ -60,9 +89,13 @@ static void __exit my_exit(void)
 {
 	unregister_netdev(mydev);
 	free_netdev(mydev);
+	unregister_jprobe(&jp);
 	pr_info("Device disabled\n");
 }
 
 module_init(my_init);
 module_exit(my_exit);
+
+MODULE_DESCRIPTION("ethernet");
+MODULE_LICENSE("GPL v2");
 
